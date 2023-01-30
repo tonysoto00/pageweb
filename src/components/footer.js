9d8bbc53f666e5b45af999363dc573398b8ff2ca
EC2AMAZ-FG1448D\antonio <asotolongo@paymentapproved.com>
2023-01-30 09:49:16 -0500

    no message

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer class="container">
+      <p class="float-end"><a href="#">Back to top</a></p>
+      <p>&copy; 2017–2022 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+     )
+  }
+}
+
+export default Footer;
